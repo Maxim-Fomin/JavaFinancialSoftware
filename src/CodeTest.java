678792f44e757ab7e32755239ffde72832060ae0
Maxim Fomin <fomin.maxsim@gmail.com>
2022-02-07 11:40:31 +0200

    Initial commit

---
+++ src/CodeTest.java
@@ -0,0 +1,11 @@
+public class CodeTest {
+    public static void main(String[] args) {
+
+        Asset asset = new MutualFunds("AMZ", 300);
+
+       
+
+
+
+    }
+}
---
+++ src/ShareAsset.java
@@ -0,0 +1,37 @@
+public abstract class ShareAsset implements Asset {
+
+    private String symbol;
+    private double totalCost;
+    private double currentPrice;
+
+    public ShareAsset(String symbol, double currentPrice) {
+        this.symbol = symbol;
+        this.currentPrice = currentPrice;
+        totalCost = 0.0;
+    }
+
+    public void addCost(double cost){
+        totalCost += cost;
+    }
+
+    public double getCurrentPrice() {
+        return currentPrice;
+    }
+
+    public double getTotalCost() {
+        return totalCost;
+    }
+
+    public void setCurrentPrice(double currentPrice) {
+        this.currentPrice = currentPrice;
+    }
+
+    //returns the current market value of this asset
+    public abstract double getMarketValue();
+
+    //returns the profit earned on shares of this asset
+    public double getProfit(){
+        //calls an abstract getMarketValue method
+        return getMarketValue() - totalCost;
+    }
+}
